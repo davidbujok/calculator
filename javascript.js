67eb6426973e4f8458ba2c648f93a724776ed0ff
davidbujok <davebujok@gmail.com>
2023-05-13 12:20:57 +0100

    Web layout, functions to make calculations
    Functions are not wired with the layout

---
+++ javascript.js
@@ -0,0 +1,21 @@
+const calculationParts = {}
+
+const simpleMathFunctions = {
+    add: (firstNumber, secondNumber) => firstNumber + secondNumber, 
+    subtract: (firstNumber, secondNumber) => firstNumber - secondNumber, 
+    multiply: (firstNumber, secondNumber) => firstNumber * secondNumber, 
+    divide: (firstNumber, secondNumber) => firstNumber / secondNumber, 
+}
+
+
+// calculationParts.firstNumber = parseInt(prompt("First Number"));
+// let operator = prompt("What do you want to do with numbers?");
+// calculationParts.secondNumber = parseInt(prompt("Second Number"));
+// console.log(typeof(calculationParts.firstNumber));
+// console.log(typeof(calculationParts.secondNumber));
+
+// console.log(simpleMathFunctions[operator](calculationParts.firstNumber, calculationParts.secondNumber));
+
+// function operate() {
+
+// };
