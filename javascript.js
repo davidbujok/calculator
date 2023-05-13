1a360f0414763f5928d002d06eb43936a5d4ee15
davidbujok <davebujok@gmail.com>
2023-05-13 13:19:21 +0100

    Add function displaying clicked number on screen.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,16 @@
+const buttons = document.querySelectorAll('.button.grey');
+const screen = document.querySelector('.screen');
+const numbers = {
+    one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8,
+    nine: 9, zero: 0
+};
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+            const number = numbers[e.target.classList[2]];
+            screen.textContent = number;
+    })
+});
+
 const calculationParts = {}
 
 const simpleMathFunctions = {
@@ -6,6 +19,8 @@ const simpleMathFunctions = {
     multiply: (firstNumber, secondNumber) => firstNumber * secondNumber, 
     divide: (firstNumber, secondNumber) => firstNumber / secondNumber, 
 }
+
+
 
 
 // calculationParts.firstNumber = parseInt(prompt("First Number"));
