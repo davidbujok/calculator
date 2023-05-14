21fda8abef5360acca417e64a7d6695c5b8e764c
davidbujok <davebujok@gmail.com>
2023-05-14 13:42:30 +0100

    Wire js functions with web user interface
    Add, subtract, multiply and divide wired
    to corresponding buttons of user interface.
    Result of each calculation rounds to five
    decimal places.

--- javascript.js
+++ javascript.js
@@ -1,36 +1,81 @@
 const buttons = document.querySelectorAll('.button.grey');
-const screen = document.querySelector('.screen');
+const calculationButtons = document.querySelectorAll('.button.darkGrey')
+const screenCalculation = document.querySelector('.calculation');
+const screenResult = document.querySelector('.result');
+const calculate = document.querySelector('#calculate');
+const dot = document.querySelector('.dot');
+
 const numbers = {
     one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8,
     nine: 9, zero: 0
 };
-buttons.forEach(button => {
-    button.addEventListener('click', (e) => {
-            const number = numbers[e.target.classList[2]];
-            screen.textContent = number;
+
+let results = [];
+let result;
+let firstNumber;
+let secondNumber;
+let lastOperator;
+let newOperator;
+let number;
+
+buttons.forEach(e => {
+    e.addEventListener('click', (e) => {
+        number = numbers[e.target.classList[2]]; // 1 to 9 number
+        screenCalculation.textContent += number;
+        numberEntered = parseFloat(screenCalculation.textContent); // Number from screen  
+        console.log("🚀 ~ numberEntered:", numberEntered);
+        })
+    });
+dot.addEventListener('click', (e) => {
+    screenCalculation.textContent += "." 
+})
+    
+calculationButtons.forEach(e => {
+    e.addEventListener('click', (e) => {
+        screenCalculation.textContent = "";
+        lastOperator = newOperator;
+        newOperator = e.target.classList[2];   
+        // console.log("🚀 ~ operator:", operator);
+        if (firstNumber !== undefined) {
+            secondNumber = numberEntered;
+            result = simpleMathFunctions[lastOperator](firstNumber, secondNumber);
+            console.log("🚀 ~ result:", result)
+            results.push(result);
+            firstNumber = result;
+            let displayed = parseFloat(results.slice(-1));
+            screenResult.textContent = displayed.toFixed(5);
+            secondNumber;
+        } else {
+            firstNumber = numberEntered;
+            console.log("🚀 ~ firstNumber:", firstNumber);
+        }
     })
-});
-
-const calculationParts = {}
-
-const simpleMathFunctions = {
-    add: (firstNumber, secondNumber) => firstNumber + secondNumber, 
-    subtract: (firstNumber, secondNumber) => firstNumber - secondNumber, 
-    multiply: (firstNumber, secondNumber) => firstNumber * secondNumber, 
-    divide: (firstNumber, secondNumber) => firstNumber / secondNumber, 
-}
-
-
-
-
-// calculationParts.firstNumber = parseInt(prompt("First Number"));
-// let operator = prompt("What do you want to do with numbers?");
-// calculationParts.secondNumber = parseInt(prompt("Second Number"));
-// console.log(typeof(calculationParts.firstNumber));
-// console.log(typeof(calculationParts.secondNumber));
-
-// console.log(simpleMathFunctions[operator](calculationParts.firstNumber, calculationParts.secondNumber));
-
-// function operate() {
-
-// };
+})
+    
+calculate.addEventListener('click', (e) => {
+    // enteredNumber = parseInt(screen.textContent);
+    if (firstNumber !== undefined) {
+        secondNumber = numberEntered;
+        result = simpleMathFunctions[newOperator](firstNumber, secondNumber);
+        console.log("🚀 ~ result:", result)
+        results.push(result);
+        firstNumber = result;
+        displayed = parseFloat(results.slice(-1));
+        screenResult.textContent = displayed.toFixed(5);
+        secondNumber;
+    }
+    });
+    
+const clear = document.querySelector('#ac');
+clear.addEventListener('click', (e) => {
+        screen.textContent = "";
+        storingNumbers = [];
+        console.log(storingNumbers);
+    })
+    
+    const simpleMathFunctions = {
+        add: (firstNumber, secondNumber) => firstNumber + secondNumber, 
+        subtract: (firstNumber, secondNumber) => firstNumber - secondNumber, 
+        multiply: (firstNumber, secondNumber) => firstNumber * secondNumber, 
+        divide: (firstNumber, secondNumber) => firstNumber / secondNumber, 
+    }
